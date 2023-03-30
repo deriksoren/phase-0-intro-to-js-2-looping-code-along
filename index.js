19a8f7d8a3f4007ca3dc3ced72e70525ffdb7397
Erik Sorensen <deriksoren@gmail.com>
2023-03-29 20:01:28 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const finalArray = [];
+let names = ["Erik", "Kate", "Emma"];
+let occ = "Hannukah";
+function writeCards (names, occ) {
+    for (let i = 0; i <names.length; i ++) {
+        finalArray.push (`Thank you, ${names[i]}, for the wonderful ${occ} gift!`)
+    }
+return finalArray
+}
+
+let count = 0 ;
+function countDown (count) {
+    while (count >= 0) {
+        console.log (count --);
+    }
+}
